b9e6852e98b127c0806870e7e20213ac66208f55
junior <crossci@163.com>
2017-08-06 15:20:41 +0800

    宽字符转窄字符

--- stringutils/stringutils.h
+++ stringutils/stringutils.h
@@ -1,10 +1,14 @@
 #include <string>
+#include "..\base\define.h"
 
-class stringutils
+class DLL_SAMPLE_API stringutils
 {
 public:
 	static void converToUpper(std::string& src);
 
 	static void converToLower(std::string& src);
 
+	static std::string ws2s(const std::wstring& ws);
+
+	static std::wstring s2ws(const std::string& s);
 };
